9ff61c680983cf468d18d5af47bc356782c4a236
CharLee Racine <charleeracine@yahoo.com>
2024-03-18 14:07:41 -0600

    to myzip

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -18,7 +18,52 @@ class Array
     end
     array
   end
+
+  def my_any?(&prc)
+    self.my_each do |ele|
+      return true if prc.call(ele)
+    end
+    false
+
+  end
+
+  def my_all?(&prc)
+    self.my_each do |ele|
+      return false if !prc.call(ele)
+    end
+    true
+  end
+
+  def my_flatten
+    return [] if self == []
+
+    res = []
+
+    self.each do |ele|
+      if ele.is_a?(Array)
+        res += ele.my_flatten
+      else 
+        res << ele
+      end
+    end
+    res
+  end
+
+  def my_zip(*arrays)
+    res = Array.new(self.length) {Array.new}
+  end
+
 end
+
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+# a = [1, 2, 3]
+# p a.my_all? { |num| num > 1 } # => false
+# p a.my_all? { |num| num < 4 } # => true
+
+# a = [1, 2, 3]
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
 
 
 # # calls my_each twice on the array, printing all the numbers twice.
