ed7e548e15b3c9ca3c39d65fc3ce481b84b4de93
CharLee Racine <charleeracine@yahoo.com>
2024-03-18 15:07:26 -0600

    done with myrotate

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -55,8 +55,8 @@ class Array
     res = Array.new(self.length) {Array.new}
     self.each_with_index {|ele, idx| res[idx] << ele}
     arrays.each do |array|
-      array.each_with_index do |ele, idx|
-        res[idx] << ele
+      (0...self.length).each do |i|
+        res[i] << array[i]
       end
     end
 
@@ -71,18 +71,30 @@ class Array
     res
   end
 
+  def my_rotate(num = 1)
+
+    remainder = num % self.length
+    self[remainder..-1] + self[0...remainder]
+    
+  end
+
 end
 
+a = [ "a", "b", "c", "d" ]
+p a.my_rotate         #=> ["b", "c", "d", "a"]
+p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
 
-a = [ 4, 5, 6 ]
-b = [ 7, 8, 9 ]
-# p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+# a = [ 4, 5, 6 ]
+# b = [ 7, 8, 9 ]
+# # p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+# p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
 
-c = [10, 11, 12]
-d = [13, 14, 15]
-p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
 
 # p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
